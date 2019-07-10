625bb678d2d5b8cd938e0a2e3756a057a848a93f
Leandro Naidhig <48226434+Leandro-Naidhig@users.noreply.github.com>
2019-07-10 02:58:30 -0300

    Update script.sh

--- app2/script.sh
+++ app2/script.sh
@@ -1,4 +1,4 @@
 #Criação do database para o app2
 sudo docker volume create metrics_containers
-sudo docker service create -d --name data_containers -p 8086:8086 --mount source=metrics_containers,target=/var/lib/influxdb --network ClusterNet influxdb
+sudo docker service create -d --name data_containers -p 8087:8086 --mount source=metrics_containers,target=/var/lib/influxdb --network ClusterNet influxdb
 sleep 60; curl -X POST -G http://localhost:8086/query --data-urlencode "q=CREATE DATABASE prometheus"
